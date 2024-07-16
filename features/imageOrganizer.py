a89913ec2dfdcef2a55b83a66efd5cac454537cd
SYH <yihyunsong0815@icloud.com>
2024-07-16 23:22:49 +0900

    Update imageOrganizer.py

--- features/imageOrganizer.py
+++ features/imageOrganizer.py
@@ -8,8 +8,7 @@ downloadDir = "./static/organizerTest/download"  # 임시 디렉토리임
 serverDir = "./static/organizerTest/server"  # 임시 디렉토리임
 
 while True:
-    if len(os.listdir(downloadDir)) > 0:
-
+     if len(os.listdir(downloadDir)) > 0:
         dirContent = os.listdir(downloadDir)
         dirContent.sort()
         imgFileName = os.path.splitext(dirContent[0])[0]  # 교체필요
@@ -17,22 +16,16 @@ while True:
         UUID = imgFileName[-36:]
         copies = imgFileName[4:5]
         print(copies, UUID)
-
         try:
             os.mkdir(f"{serverDir}/{UUID}")
         except FileNotFoundError:
             os.mkdir(f"{serverDir}")
             os.mkdir(f"{serverDir}/{UUID}")
-
         shutil.move(os.path.join(downloadDir, dirContent[0]), os.path.join(f"{serverDir}/{UUID}", dirContent[0]))
-
         for i in range(1, len(dirContent)):
             shutil.move(os.path.join(downloadDir, dirContent[i]), os.path.join(f"{serverDir}/{UUID}", dirContent[i]))
         print(f"File Moved. >> Session {UUID}")
-
         img = PIL.Image.open(f"{serverDir}/{UUID}/{dirContent[0]}").convert("RGB")
         img.save(f"{serverDir}/{UUID}/{imgFileName}.pdf")
         print(f"pdf file created. >> Session{UUID}")
-
         # printingFunc.pysidePrint(f"{serverDir}/{UUID}/{imgFileName}.pdf", copies)
-
