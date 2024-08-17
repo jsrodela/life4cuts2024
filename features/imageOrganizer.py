6fb176e029199459867a4d75acf16400ac187c86
SYH <yihyunsong@outlook.com>
2024-08-18 00:11:56 +0900

    Update imgOrganizer

--- features/imageOrganizer.py
+++ features/imageOrganizer.py
@@ -1,8 +1,7 @@
 import os
 import shutil
-import PIL
-from PIL import Image
-import printingFunc
+# import PIL
+# from PIL import Image
 
 downloadDir = "./static/organizerTest/download"  # 임시 디렉토리임
 serverDir = "./static/organizerTest/server"  # 임시 디렉토리임
@@ -11,11 +10,13 @@ while True:
      if len(os.listdir(downloadDir)) > 0:
         dirContent = os.listdir(downloadDir)
         dirContent.sort()
-        imgFileName = os.path.splitext(dirContent[0])[0]  # 교체필요
+        fileName = os.path.splitext(dirContent[0])[0] # 교체필요
 
-        UUID = imgFileName[-36:]
-        copies = imgFileName[4:5]
+        UUID = fileName[-36:]
+        copies = fileName[:5].split("-")[-1]
+
         print(copies, UUID)
+
         try:
             os.mkdir(f"{serverDir}/{UUID}")
 
@@ -23,13 +24,13 @@ while True:
             os.mkdir(f"{serverDir}")
             os.mkdir(f"{serverDir}/{UUID}")
 
-        shutil.move(os.path.join(downloadDir, dirContent[0]), os.path.join(f"{serverDir}/{UUID}", dirContent[0]))
+        shutil.move(f"{downloadDir}/{dirContent[0]}", f"{serverDir}/{UUID}/{dirContent[0]}")
 
         for i in range(1, len(dirContent)):
-            shutil.move(os.path.join(downloadDir, dirContent[i]), os.path.join(f"{serverDir}/{UUID}", dirContent[i]))
+            shutil.move(f'{downloadDir}/{dirContent[i]}', f"{serverDir}/{UUID}/{dirContent[i]}")
         print(f"File Moved. >> Session {UUID}")
 
-        img = PIL.Image.open(f"{serverDir}/{UUID}/{dirContent[0]}").convert("RGB")
-        img.save(f"{serverDir}/{UUID}/{imgFileName}.pdf")
-        print(f"pdf file created. >> Session{UUID}")
+        # img = PIL.Image.open(f"{serverDir}/{UUID}/{dirContent[0]}").convert("RGB")
+        # img.save(f"{serverDir}/{UUID}/{fileName}.pdf")
+        # print(f"pdf file created. >> Session{UUID}")
         # printingFunc.pysidePrint(f"{serverDir}/{UUID}/{imgFileName}.pdf", copies)
