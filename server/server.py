2ecf92c00606a015030517f5fbc53c1619222f29
Yoon_JW <jiwookyoon0744@gmail.com>
2024-08-23 23:09:14 +0900

    resolve socketio server not working

--- server/server.py
+++ server/server.py
@@ -1,10 +1,10 @@
 import json, base64
 from PIL import Image
 from io import BytesIO
-import print # 프린트 모듈
+import printer # 프린트 모듈
 import upload # QR코드 및 업로드 모듈
 import combine # 사진 & 프레임 합성 함수
-import socketio
+import socketio, eventlet
 
 # Socket.IO 서버 생성
 sio = socketio.Server()
@@ -17,20 +17,31 @@ def connect(sid, environ):
 # 메시지 이벤트 핸들러
 @sio.event
 def message(sid, data):
+
     data = json.loads(data)
     people = data.people # int
     frame = data.frame # int
     photo = [data.p1, data.p2, data.p3, data.p4] # base64 string
     for i in range(4): # base64 로 받은 이미지 디코딩
         photo[i] = Image.open(BytesIO(base64.b64decode(photo[i])))
+
+    print(f'인원수: {people}, 프레임: {frame}')
+
     code = upload.pre_code()
     qrcode = upload.gen_qrcode(code)
+
     final_image = combine.combine_photo(photo, qrcode, frame)
     final_image.save(f'./media/final-{code}.png', 'png')
-    print.print_printer(f'./media/final-{code}.png', people)
+
+    printer.print_printer(f'./media/final-{code}.png', people)
     upload.post_file(code, f'./media/final-{code}.png')
 
-# 실행할 때 웹 소켓 서버 시작ß
+# 연결 해제 이벤트 핸들러
+@sio.event
+def disconnect(sid):
+    print('클라이언트가 연결 해제되었습니다:', sid)
+
+# 실행할 때 웹 소켓 서버 시작
 if __name__ == '__main__':
     app = socketio.WSGIApp(sio)
-    socketio.server.SocketIOServer(('localhost', 8000), app).serve_forever()
+    eventlet.wsgi.server(eventlet.listen(('localhost', 4000)), app)
